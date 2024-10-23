e5df8ec5c096c72e099a65500c60a5dc6bf392df
Ricardo, Gabriel <gabriel.ricardo@boeing.com>
2024-10-23 14:53:34 -0300

    added user input component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { UserInputModule } from './user-input/user-input.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent
   ],
-  imports: [BrowserModule],
+  imports: [BrowserModule, UserInputModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
