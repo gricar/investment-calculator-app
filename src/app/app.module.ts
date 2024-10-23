5a2a2eb79b38c797bb372c88d773fd0256305fb0
Ricardo, Gabriel <gabriel.ricardo@boeing.com>
2024-10-23 14:57:04 -0300

    added investment results table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserInputModule } from './user-input/user-input.module';
+import { InvestmentResultsComponent } from './investment-results/investment-results.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    InvestmentResultsComponent
   ],
   imports: [BrowserModule, UserInputModule],
   bootstrap: [AppComponent]
